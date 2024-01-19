18610b8775d8419f9d252b3bd35b30f94b9314c0
= <sohamom7@gmail.com>
2024-01-19 05:03:29 -0800

    static keyword

--- index.php
+++ index.php
@@ -2,22 +2,23 @@
 <html lang="en">
 <body>
     <?php
-        // echo "Hello World";
-        // echo phpversion();
-        // $x = "Sohm";
-        // $y = "Bharti";
-        // echo $x . $y;
 
-        // $z = 30;
-        // // echo $z;
-        // echo "<br>";
-        // echo var_dump($z); // type of data
+        /*
+        // declare variables and printing
+    
+        echo "Hello World";
+        echo phpversion();
+        $x = "Sohm";
+        $y = "Bharti";
+        echo $x . $y;
 
-        // $z = 'soham';
-        // // echo $z;
-        // echo "<br>";
-        // echo var_dump($z);
+        $z = 30;
+        // echo $z;
+        echo "<br>";
+        echo var_dump($z); // type of data
 
+        
+        // global variables are only accessible outside a function but we can use them using global keyword
         $a = 10;
         $b = 20;
 
@@ -26,7 +27,33 @@
             $b = $a + $b;
         }
         fun();
-        echo $b;
+        echo $b; // output 30
+        
+        */
+
+        // generally a varible inside a function is deleted after it's use but static keyword is used when we want to use the variable further ahead
+        function test(){
+            $a = 0;
+            echo $a; 
+            $a++;
+        }
+        test(); // output 0
+        test(); // output 0
+        test(); // output 0
+        test(); // output 0
+        test(); // output 0
+        test(); // output 0
+
+        function test1(){
+            static $a = 0;
+            echo $a; 
+            $a++;
+        }
+        test1(); // output 0
+        test1(); // output 1
+        test1(); // output 2
+        test1(); // output 3
+
 
     ?>
 </body>
