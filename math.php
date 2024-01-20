a467c67e5ba2ac5973764ac4e7b816cc8b5e7a43
= <sohamom7@gmail.com>
2024-01-20 01:46:42 -0800

    php math done

---
+++ math.php
@@ -0,0 +1,23 @@
+<?php
+
+// echo pi();
+
+// echo min(2,4,56,78,-3,-90,-4,0);
+
+// echo max(-100,4,7,88,433,90,2);
+
+// echo round(2.4);
+
+// echo floor(2.99);
+
+// echo ceil(2.0001);
+
+// echo sqrt(81);
+
+// echo abs(2.98);
+
+// echo rand();
+
+echo rand(0,100);
+
+
