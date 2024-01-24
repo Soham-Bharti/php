57800301ca8f9d3290c1c3a89ede11bf7bbf95df
Soham Bharti <sohamom7@gmail.com>
2024-01-24 11:28:47 +0530

    Cast To Array done + Modified

--- Casting/castToArray.php
+++ Casting/castToArray.php
@@ -24,3 +24,27 @@ var_dump((array)$null); // array(0) { }
 echo "<br>";
 var_dump((array)$nan); // array(1) { [0]=> float(NAN) }
 echo "<br>";
+
+// When converting into arrays, most data types converts into an indexed array with one element.
+
+// NULL values converts to an empty array object.
+
+// Objects converts into associative arrays where the property names becomes the keys and the property values becomes the values:
+
+// Converting Objects into Arrays:
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("red", "Volvo");
+  
+  $myCar = (array) $myCar;
+  var_dump($myCar); // array(2) { ["color"]=> string(3) "red" ["model"]=> string(5) "Volvo" }
