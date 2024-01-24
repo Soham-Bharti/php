45b399989b91fce067e15b58027a6698efc0a2d3
Soham Bharti <sohamom7@gmail.com>
2024-01-24 11:54:53 +0530

    acos() done

---
+++ Math/MathFunctions/acos.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// PHP acos() Function
+// The acos() function returns the arc cosine of a number.
+
+// Tip: acos(-1) returns the value of Pi.
+
+// Syntax
+// acos(number); -> Required. Specifies a number in range -1 to 1
+
+echo (acos(0.64) . "<br>"); // 0.87629806116834
+echo (acos(-0.4) . "<br>"); // 1.9823131728624
+echo (acos(0) . "<br>"); // 1.5707963267949
+echo (acos(-1) . "<br>"); // 3.1415926535898
+echo (acos(1) . "<br>"); // 0
+echo (acos(2)) . "<br>"; // NAN
+echo (acos(-2)); // NAN
