8ae7faa2b7071bb1e15217e93e915b8f39076473
Soham Bharti <sohamom7@gmail.com>
2024-01-24 16:16:00 +0530

    sqrt() done

---
+++ Math/MathFunctions/sqrt.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// PHP sqrt() Function
+// The sqrt() function returns the square root of a number.
+
+echo (sqrt(0) . "<br>"); // 0
+echo (sqrt(1) . "<br>"); // 1
+echo (sqrt(1.98) . "<br>"); // 1.407124727947
+echo (sqrt(-1.98) . "<br>"); // NAN
+echo (sqrt(9) . "<br>"); // 3
+echo (sqrt(0.64) . "<br>"); // 0.8
+echo (sqrt(-9)); // NAN
