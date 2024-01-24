640c498bcbb890009fffae0650951ee18dced28e
Soham Bharti <sohamom7@gmail.com>
2024-01-24 15:36:39 +0530

    mt_getrandmax() done

---
+++ Math/MathFunctions/mt_getrandmax.php
@@ -0,0 +1,7 @@
+<?php
+
+
+// PHP mt_getrandmax() Function
+// The mt_getrandmax() function returns the largest possible value that can be returned by mt_rand().
+
+echo(mt_getrandmax()); // 2147483647
