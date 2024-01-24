ba86101ef27f46e77524ec26fd67e667d24249a3
Soham Bharti <sohamom7@gmail.com>
2024-01-24 15:03:19 +0530

    hypot() done

---
+++ Math/MathFunctions/hypot.php
@@ -0,0 +1,10 @@
+<?php
+
+// The hypot() function calculates the hypotenuse of a right-angle triangle.
+
+// Tip: This function is equivalent to sqrt(x*x + y*y).
+
+echo hypot(3, 4) . "<br>"; // 5
+echo hypot(4, 6) . "<br>"; // 7.211102550928
+echo hypot(1, 3) . "<br>"; // 3.1622776601684
+echo sqrt(3 * 3 + 4 * 4); // 5
