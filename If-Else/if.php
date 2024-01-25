7309e7c6fea55deb6c4d4c474833268f3b291eaa
Soham Bharti <sohamom7@gmail.com>
2024-01-25 16:20:43 +0530

    If-else done

---
+++ If-Else/if.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// PHP - The if Statement
+// The if statement executes some code if one condition is true.
+
+$t = 14;
+
+if ($t < 20) {
+    echo "Have a good day! <br>";
+}
+if ($t < 20) echo "Have a good day! <br>";
+
+// ==	Equal	Returns true if the values are equal	
+// ===	Identical	Returns true if the values and data types are identical	
+// !=	Not equal	Returns true if the values are not equal	
+// <>	Not equal	Returns true if the values are not equal	
+// !==	Not identical	Returns true if the values or data types are not identical	
+// >	Greater than	Returns true if the first value is greater than the second value	
+// <	Less than	Returns true if the first value is less than the second value	
+// >=	Greater than or equal to	Returns true if the first value is greater than, or equal to, the second value	
+// <=	Less than or equal to	Returns true if the first value is less than, or equal to, the second value
+
+if (10 !== '10') echo 'Same but not identical';
+
+
+
+// and	And	True if both conditions are true	
+// &&	And	True if both conditions are true	
+// or	Or	True if either condition is true	
+// ||	Or	True if either condition is true	
+// xor	Xor	True if either condition is true, but not both	
+// !	Not	True if condition is not true
