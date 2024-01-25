9c28b4c9faa9768b5bfe5c7b955bb22a347ffa83
Soham Bharti <sohamom7@gmail.com>
2024-01-25 11:18:24 +0530

    Alternate method done

--- Strings/Test/Ques2.php
+++ Strings/Test/Ques2.php
@@ -4,11 +4,13 @@
 
 $givenStr = "hello how are you";
 
-$total = 0;
+// $total = 0;
 
-for ($i = 0; $i < strlen($givenStr); $i++) {
-    if ($givenStr[$i] == 'a' || $givenStr[$i] == 'e' || $givenStr[$i] == 'o' || $givenStr[$i] == 'u') {
-        $total++;
-    }
-}
-echo "Ans is " . $total;
+// for ($i = 0; $i < strlen($givenStr); $i++) {
+//     if ($givenStr[$i] == 'a' || $givenStr[$i] == 'e' || $givenStr[$i] == 'o' || $givenStr[$i] == 'u') {
+//         $total++;
+//     }
+// }
+
+echo substr_count($givenStr, 'a') + substr_count($givenStr, 'e') + substr_count($givenStr, 'i') + substr_count($givenStr, 'o') + substr_count($givenStr, 'u');
+// echo "Ans is " . $total;
