a1a1b74612b44ea6b003d14e28ea3543e3368361
Soham Bharti <sohamom7@gmail.com>
2024-01-25 14:26:38 +0530

    Arithmetic Operations done

---
+++ Operators/Arithmetic.php
@@ -0,0 +1,20 @@
+<?php
+
+// Arithmetic operations
+
+// +	Addition	$x + $y	Sum of $x and $y	
+// -	Subtraction	$x - $y	Difference of $x and $y	
+// *	Multiplication	$x * $y	Product of $x and $y	
+// /	Division	$x / $y	Quotient of $x and $y	
+// %	Modulus	$x % $y	Remainder of $x divided by $y	
+// **	Exponentiation	$x ** $y	Result of raising $x to the $y'th power
+
+$x = 10;
+$y = 20;
+
+echo $x + $y . "<br>"; // 30
+echo $x - $y . "<br>"; // -10 
+echo $x * $y . "<br>"; // 200
+echo $x / $y . "<br>"; // 0.5
+echo $x % $y . "<br>"; // 10
+echo $x ** $y . "<br>"; // 1.0E+20
