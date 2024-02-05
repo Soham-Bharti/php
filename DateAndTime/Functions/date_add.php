07a8a0199080fdb17339840141b0d0f5ef29626c
Soham Bharti <sohamom7@gmail.com>
2024-02-05 13:10:48 +0530

    date_add() done + Modified

--- DateAndTime/Functions/date_add.php
+++ DateAndTime/Functions/date_add.php
@@ -7,8 +7,8 @@
 // date_add(object, interval)
 
 $date = date_create("2024-02-5");
-date_add($date, date_interval_create_from_date_string("4 days"));
-echo date_format($date, "Y-m-d") . "<br>"; // 2024-02-09
+date_add($date, date_interval_create_from_date_string("4 days 5hours 6min 10sec"));
+echo date_format($date, "Y-m-d h:i:s") . "<br>"; // 2024-02-09 05:06:10
 
 date_add($date, date_interval_create_from_date_string("365 days"));
 echo date_format($date, "Y-m-d") . "<br>"; // 2025-02-08
