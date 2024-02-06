af3012a0cfc8f0379d99c15365310170c4536c38
Soham Bharti <sohamom7@gmail.com>
2024-02-06 10:47:26 +0530

    File Handling with r+ mode done

---
+++ FileHandling/Practice/r+/readAndWrite.php
@@ -0,0 +1,40 @@
+<?php
+
+// r+ -> Open a file for read/write. File pointer starts at the beginning of the file
+
+// *** For reading the text that has been newly appended we have to open the file again ***
+
+
+// $myFile = fopen('demo.txt', 'r+') or die('Can not open file');
+// // echo fread($myFile, filesize($myFile)); // Fatal error: Uncaught TypeError: filesize(): Argument #1 ($filename) must be of type string, resource given in
+// echo fread($myFile, filesize('demo.txt')); // Hey there, I am already existing text!27
+// echo fwrite($myFile, "I am written text by server");
+// echo fread($myFile, filesize('demo.txt')); // Hey there, I am already existing text!27
+// fclose($myFile);
+
+// Why the added text is not displayed??
+
+
+$myFile = fopen('demo.txt', 'r+') or die('Can not open file');
+echo fread($myFile, filesize('demo.txt')); // Hey there, I am already existing text!27
+echo "<br>";
+echo fwrite($myFile, "I am written text by server"); // 27
+fseek($myFile, 0);
+echo "<br>";
+echo fread($myFile, filesize('demo.txt')); // Hey there, I am already existing text!27
+echo "<br>";
+fclose($myFile);
+// Output
+// Hey there, I am already existing text!
+// 27
+// Hey there, I am already existing text!
+
+
+$myFile = fopen('demo.txt', 'r+') or die('Cannot open file'); // Open file for reading
+while (!feof($myFile)) {
+    echo fgets($myFile) . "<br>";
+}
+fclose($myFile);
+
+// Output
+// Hey there, I am already existing text!I am written text by server
