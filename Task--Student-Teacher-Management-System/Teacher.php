38d37267d8b60b600a08ec5bb9f97f83d9962286
Soham Bharti <=>
2024-02-20 18:25:41 +0530

    Task by Sagar, Teacher-Student Managing System done

---
+++ Task--Student-Teacher-Management-System/Teacher.php
@@ -0,0 +1,58 @@
+<?php
+require './Traits/showStudentMarks.php';
+$studentDetails = [
+    '65d48892162fa' => array('name' => 'SOHAM BHARTI', 'mob' => 8987675654, 'age' => 22, 'email' => 'soham.silversky@gmail.com'),
+];
+$studentMarks = [
+    '65d48892162fa' => array('maths' => 92, 'english' => 83, 'hindi' => 88),
+];
+
+class Teacher
+{
+    use showStudentMarks;
+    private $name;
+    private $mob;
+    private $age;
+    private $email;
+    private $id;
+    function enrollStudent($name, $mob, $age, $email)
+    {
+        global $studentDetails;
+        $this->name = $name;
+        $this->mob = $mob;
+        $this->age = $age;
+        $this->email = $email;
+        $this->id = uniqid("");
+        $newStudent = array('name' => $this->name, 'mob' => $this->mob, 'age' => $this->age, 'email' => $this->email);
+        $studentDetails[$this->id] = $newStudent;
+    }
+    function showStudentDetails($roll)
+    {
+        global $studentDetails;
+        if (array_key_exists($roll, $studentDetails)) {
+            echo "Printing " . $studentDetails[$roll]['name'] . "'s details:<br>";
+            print_r($studentDetails[$roll]);
+        } else echo "Student not found!";
+    }
+    function showStudentsDetails()
+    {
+        global $studentDetails;
+        print_r($studentDetails);
+    }
+
+    function showStudentsMarks()
+    {
+        global $studentMarks;
+        print_r($studentMarks);
+    }
+
+    function addMarks($roll, $mathsMarks, $englishMarks, $hindiMarks)
+    {
+        global $studentMarks, $studentDetails;
+        if (array_key_exists($roll, $studentDetails)) {
+            $marksArray = array('maths' => $mathsMarks, 'english' => $englishMarks, 'hindi' => $hindiMarks);
+            $studentMarks[$roll] = $marksArray;
+        } else echo "Student not found!";
+    }
+
+}
