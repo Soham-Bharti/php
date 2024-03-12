c9c148e4c5f030dab880ea8cc2dfafad33ecfdc0
Soham Bharti <=>
2024-03-12 19:07:27 +0530

    User Registration page done including validation - file upload, linking and db store left

---
+++ Pages/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <marquee direction="left" class='marquee'>Welcome to Web Employee Tracker</marquee>
+    <div class="container">
+        <form action="register.php">
+            <input type="submit" name="reg-submit" value="Register">
+        </form>
+        <form action="login.php">
+            <input type="submit" name="login-submit" value="Login">
+        </form>
+    </div>
+</body>
+
+</html>
