6445db32cce7027088db0f60f33f466b941bf81b
Soham Bharti <=>
2024-03-20 13:09:46 +0530

    footer fixed to bottom and also added class in body of all pages to support the footer in staying always at the bottom

--- Pages/views/footer.php
+++ Pages/views/footer.php
@@ -1,4 +1,4 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center m-3 p-3 border-top">
+<footer class="d-flex flex-wrap justify-content-between align-items-center m-3 p-3 border-top mt-auto">
     <p class="mb-0 text-body-secondary">Copyright &copy; 2023 - <?php echo date("Y") ?>, All Rights Reserved</p>
 
     <a href="../common/home.php" class="col-1 svg">
