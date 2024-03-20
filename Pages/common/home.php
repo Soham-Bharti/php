c2e39695568fd4d8d9b394b9a7f23ea74c299374
Soham Bharti <=>
2024-03-20 13:10:49 +0530

    added class in body to support footer stay at bottom always

--- Pages/common/home.php
+++ Pages/common/home.php
@@ -19,7 +19,7 @@ unset($_SESSION['role']);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class = 'd-flex flex-column min-vh-100'>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid d-flex align-items-center justify-content-between">
             <a href="home.php" class="svg text-decoration-none d-flex align-items-center">
